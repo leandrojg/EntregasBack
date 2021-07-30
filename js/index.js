24b07541cbabbccc36a5272edcb967a8b59eafec
leandrojg <79561670+leandrojg@users.noreply.github.com>
2021-07-30 06:03:15 -0300

    changes

--- js/index.js
+++ js/index.js
@@ -1,47 +1,39 @@
-
-
 let animales = "Gato, Elefante, Perro, Mono";
 let hombres = "Mariano, Raquel, Arturo, Leonel";
 let autos = "Fiat, Audi, Ferrari, Ford";
 
+//aqui deberia recibir un texto y no un array. tambien deberia recibir un el timeout como parametro.
+function mostrarPalabras(arr, i, callback) {
+  if (arr.length == i) {
+    console.log("Proceso completo");
+    console.log(`Cantidad de palabras: ${arr.length}`);
+    return;
+  }
 
-function mostrarPalabras(arr, i, callback,) {
-
-    if (arr.length == i) {
-        console.log('Proceso completo')
-        console.log(`Cantidad de palabras: ${arr.length}`)
-        return;
-    }
-
-    setTimeout(() =>{
-        callback(arr[i]);
-        mostrarPalabras(arr, i+1, callback);
-    
-    },1000)
-
+  setTimeout(() => {
+    callback(arr[i]);
+    mostrarPalabras(arr, i + 1, callback);
+  }, 1000);
 }
 
+// mostrarPalabras(animales.split(" "), 0, (e) => {
+//   console.log(e);
+// });
 
-mostrarPalabras(animales.split(' '),0,(e)=>{
-    console.log(e)
-});
+// mostrarPalabras(hombres.split(" "), 0, (e) => {
+//   console.log(e);
+// });
 
-
-mostrarPalabras(hombres.split(' '),0,(e)=>{
-    console.log(e)
-});
-
-
-mostrarPalabras(autos.split(' '),0,(e)=>{
-    console.log(e)
-});
+// mostrarPalabras(autos.split(" "), 0, (e) => {
+//   console.log(e);
+// });
 
 //probe utilizar este ejemplo de anidamiento pero no funciono :(
-
+//creo que estaba medio cerca, pero no se me ocurre facil como hacerlo con setTimeout
 // let hacerTarea2 = (msg,cb) => {
 //     console.log(msg);
 //     setTimeout(cb, 100)
-// }   
+// }
 
 // console.log('Iniciando tarea')
 // hacerTarea2(1,()=>{
@@ -50,8 +42,35 @@ mostrarPalabras(autos.split(' '),0,(e)=>{
 
 //         hacerTarea2(3,()=>{
 
-//         console.log('finalizar tarea')    
+//         console.log('finalizar tarea')
 //         })
 //     })
 // })
 
+//hago otra func aqui
+function mostrarWords(texto, cant, timeout = 1000, callback) {
+  //cant cuenta las palabras, y timeout creo que era 1s por defecto
+  const array = texto.split(" "); //es mejor ponerlo adentro y no tener que pasarlo como parametro cada vez que se llama la f.
+  let index = 0; //un contador bien clasico
+  let id = setInterval(
+    //aveces permite hacer cosas mas divertidas que setTimeout
+    () => {
+      console.log(array[index++]); //aquí voy mostrando cada palabra
+      if (index >= array.length) {
+        //con este condicional basicamente estoy usando setInterval como un while xD
+        clearInterval(id); //como buen bucle, no quiero que sea infinito
+        callback(array.length + cant); //aqui voy acumulando la cantidad de palabras
+      }
+    },
+    timeout //el segundo parametro de setInterval es el tiempo entre cada palabra.
+  );
+}
+
+//aqui viene lo divertido
+mostrarWords(animales, 0, 300, (cant) => {
+  mostrarWords(hombres, cant, 500, (cant) => {
+    mostrarWords(autos, cant, 700, (cant) => {
+      console.log("Proceso terminado, Cantidad de palabras:", cant); //callback hell como corresponde
+    });
+  });
+});
